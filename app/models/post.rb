1514085b7a198ec8a18a18cf76a776b6d5ef7cd9
Matt <M.martin747@gmail.com>
2018-10-26 16:22:08 +1000

    migration of many

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+class Post < ApplicationRecord
+    has_many :comments
+    belongs_to :user
+    has_one_attached :image
+
+    def image_tag
+        cl_image_tag(post.image.key)
+    end
+
+    def thumb_tag
+        cl_image_tag(post.image.key, :width=>100, :crop=>"fit")
+    end
+
+
+end
